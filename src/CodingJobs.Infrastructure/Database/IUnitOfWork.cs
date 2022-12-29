e6d6f9c5f3cf98db355899748275ae7f0583091e
Matt Kiggen <28730291+mattkiggen@users.noreply.github.com>
2022-12-29 10:43:26 +0200

    Add job repo to unit of work

--- src/CodingJobs.Infrastructure/Database/IUnitOfWork.cs
+++ src/CodingJobs.Infrastructure/Database/IUnitOfWork.cs
@@ -1,10 +1,11 @@
-﻿using CodingJobs.Infrastructure.Repositories;
-using CodingJobs.Infrastructure.Repositories.Company;
+﻿using CodingJobs.Infrastructure.Repositories.Company;
+using CodingJobs.Infrastructure.Repositories.Job;
 
 namespace CodingJobs.Infrastructure.Database;
 
 public interface IUnitOfWork
 {
     ICompanyRepository CompanyRepository { get; }
+    IJobRepository JobRepository { get; }
     Task<int> SaveChangesAsync();
 }
--- src/CodingJobs.Infrastructure/Database/UnitOfWork.cs
+++ src/CodingJobs.Infrastructure/Database/UnitOfWork.cs
@@ -1,5 +1,5 @@
-﻿using CodingJobs.Infrastructure.Repositories;
-using CodingJobs.Infrastructure.Repositories.Company;
+﻿using CodingJobs.Infrastructure.Repositories.Company;
+using CodingJobs.Infrastructure.Repositories.Job;
 
 namespace CodingJobs.Infrastructure.Database;
 
@@ -7,11 +7,13 @@ public class UnitOfWork : IUnitOfWork
 {
     private readonly ApplicationDbContext _context;
     public ICompanyRepository CompanyRepository { get; }
+    public IJobRepository JobRepository { get; }
 
     public UnitOfWork(ApplicationDbContext context)
     {
         _context = context;
         CompanyRepository = new CompanyRepository(_context);
+        JobRepository = new JobRepository(_context);
     }
     
     public async Task<int> SaveChangesAsync()
